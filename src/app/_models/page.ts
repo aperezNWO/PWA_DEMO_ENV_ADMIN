d4bc0376ceac30d317447b42bcdb7df9208a31c2
alejandro.perez.acosta@hotmail.com <aperezNWO>
2024-06-08 22:59:25 -0500

    [ANGULAR DEMO] - [ENV_LANDING] v[1.0.0.4] 1)[Pages] Table. a) Page entity.

---
+++ src/app/_models/page.ts
@@ -0,0 +1,10 @@
+export interface Page {
+	id               : number;
+	name             : string;
+    framework        : string;
+	frameworkVersion : string;
+    uixFramework     : string;    
+    description      : string;
+    urlPage          : string;
+	urlRepo          : string;
+}
---
+++ src/app/_models/pages.ts
@@ -0,0 +1,14 @@
+import { Page } from './page';
+
+export const PAGES: Page[] = [
+	{
+                id               : 1,
+                name             : '[PWA_DEMO - ENV_LANDING]',
+                framework        : 'Angular',
+                frameworkVersion : '17.3.8',
+                uixFramework     : 'Material',    
+                description      : 'Angular Learning - Landing Page',
+                urlPage          : 'https://apereznwo.github.io/PWA_DEMO_ENV_LANDING/',
+                urlRepo          : 'https://github.com/aperezNWO/PWA_DEMO_ENV_LANDING.git',
+	}
+];
